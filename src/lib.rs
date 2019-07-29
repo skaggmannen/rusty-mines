5308bd5b668c5d765137c382f7e4888a1ea51cf6
Fredrik Allansson <fredrik.allansson@assaabloy.com>
2019-07-30 00:54:12 +0200

    Working game with colors!

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,220 @@
 mod utils;
 
+use std::fmt;
+
 use wasm_bindgen::prelude::*;
+use js_sys::Math::random;
+
+#[wasm_bindgen]
+pub struct Board {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+#[wasm_bindgen]
+impl Board {
+    pub fn new(width: u32, height: u32) -> Board {
+        utils::set_panic_hook();
+
+        let cells = Board::create_cells(width, height);
+
+        Board{
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+
+    pub fn reveal(&mut self, row: u32, col: u32) {
+        if self.cell_at(row, col).revealed {
+            return;
+        }
+
+        if self.cell_at(row, col).flagged {
+            return;
+        }
+
+        let neighbors = self.get_neighbors(row, col);
+        let count = self.count_mines(&neighbors);
+
+        {
+            self.set_revealed(row, col, count);
+        }
+
+        if count == 0 {
+            for (row, col) in neighbors {
+                self.reveal(row, col);
+            }
+        }
+    }
+
+    pub fn done(&self) -> bool {
+        self.has_revealed_mine() || self.all_non_mines_revealed()
+    }
+
+    fn has_revealed_mine(&self) -> bool {
+        self.cells.iter()
+            .filter(|cell| cell.mine)
+            .filter(|cell| cell.revealed)
+            .count() != 0
+    }
+
+    fn all_non_mines_revealed(&self) -> bool {
+        self.cells.iter()
+            .filter(|cell| !cell.mine)
+            .filter(|cell| !cell.revealed)
+            .count() == 0
+    }
+
+    pub fn flag(&mut self, row: u32, col: u32) {
+        let cell = self.mut_cell_at(row, col);
+        cell.flagged = !cell.flagged;
+    }
+
+    pub fn get_cell(&self, row: u32, col: u32) -> Cell {
+        self.cell_at(row, col).clone()
+    }
+
+    fn set_revealed(&mut self, row: u32, col: u32, count: u8) {
+        let cell = self.mut_cell_at(row, col);
+        cell.revealed = true;
+        cell.neighbors = count;
+    }
+
+    fn index_of(&self, row: u32, col: u32) -> usize {
+        (row * self.width + col) as usize
+    }
+
+    fn cell_at(&self, row: u32, col: u32) -> &Cell {
+        &self.cells[self.index_of(row, col)]
+    }
+
+    fn mut_cell_at(&mut self, row: u32, col: u32) -> &mut Cell {
+        let idx = self.index_of(row, col);
+        &mut self.cells[idx]
+    }
+
+    fn get_neighbors(&self, row: u32, col: u32) -> Vec<(u32, u32)> {
+        let mut neighbors = Vec::new();
+
+        if row > 0 {
+            if col > 0 {
+                neighbors.push((row-1, col-1));
+            }
+            neighbors.push((row-1, col));
+            if col < self.width - 1 {
+                neighbors.push((row-1, col+1));
+            }
+        }
+
+        if col > 0 {
+            neighbors.push((row, col - 1));
+        }
+        if col < self.width - 1 {
+            neighbors.push((row, col + 1));
+        }
+
+        if row < self.height - 1 {
+            if col > 0 {
+                neighbors.push((row+1, col-1));
+            }
+            neighbors.push((row+1, col));
+            if col < self.width - 1 {
+                neighbors.push((row+1, col+1));
+            }
+        }
+
+        neighbors
+    }
+
+    fn count_mines(&self, coords: &[(u32, u32)]) -> u8 {
+        let mut count = 0;
+
+        for (row, col) in coords {
+            if self.cell_at(*row, *col).mine {
+                count += 1;
+            }
+        }
+
+        count
+    }
+
+    fn create_cells(width: u32, height: u32) -> Vec<Cell> {
+        let mut cells: Vec<Cell> = (0..width*height)
+            .map(|_| {
+                Cell::clear()
+            })
+            .collect();
+
+        // Place mines
+        for _ in 0..10 {
+            let mut idx = ( random() * ( width * height ) as f64 ).floor() as usize;
+            while cells[idx].mine {
+                idx = ( random() * ( width * height ) as f64 ).floor() as usize
+            }
+
+            cells[idx] = Cell::mine();
+        }
+
+        cells
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for row in 0..self.height {
+            for col in 0..self.width {
+                write!(f, "{}", self.cell_at(row, col))?;
+            }
+            writeln!(f, "")?;
+        }
+
+        Ok(())
+    }
+}
+
+#[wasm_bindgen]
+#[derive(Clone)]
+pub struct Cell {
+    pub revealed: bool,
+    pub flagged: bool,
+    pub mine: bool,
+    pub neighbors: u8,
+}
+
+#[wasm_bindgen]
+impl Cell {
+    pub fn mine() -> Cell {
+        Cell{
+            revealed: false,
+            flagged: false,
+            mine: true,
+            neighbors: 0,
+        }
+    }
+
+    pub fn clear() -> Cell {
+        Cell{
+            revealed: false,
+            flagged: false,
+            mine: false,
+            neighbors: 0,
+        }
+    }
+}
+
+impl fmt::Display for Cell {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "□")?;
+
+        Ok(())
+    }
+}
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
--- www/index.js
+++ www/index.js
@@ -1,3 +1,158 @@
-import * as wasm from "rust-mines";
+import { Board } from "rusty-mines";
 
-wasm.greet();
+const WIDTH = 10;
+const HEIGHT = 10;
+
+const GRID_COLOR = "#CCCCCC";
+const REVEALED_COLOR = "#888888";
+const HIDDEN_COLOR = "#EEEEEE";
+
+const CELL_SIZE = 20;
+
+const COUNT_COLORS = [
+    "#000000",
+    "#0000FF",
+    "#00FF00",
+    "#00FFFF",
+    "#FF0000",
+    "#FF00FF",
+    "#FFFF00",
+    "#FFFFFF",
+];
+
+
+let board = Board.new(WIDTH, HEIGHT);
+
+const canvas = document.getElementById("game-board");
+canvas.height = (CELL_SIZE + 1) * HEIGHT + 1;
+canvas.width = (CELL_SIZE + 1) * WIDTH + 1;
+
+
+const done = document.getElementById("done");
+
+canvas.addEventListener("click", event => {
+    if (board.done()) {
+        return;
+    }
+
+    const boundingRect = canvas.getBoundingClientRect();
+
+    const scaleX = canvas.width / boundingRect.width;
+    const scaleY = canvas.height / boundingRect.height;
+
+    const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+    const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+    const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), HEIGHT - 1);
+    const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), WIDTH - 1);
+
+    board.reveal(row, col);
+
+    render();
+});
+
+canvas.addEventListener("contextmenu", event => {
+    event.preventDefault();
+    if (board.done()) {
+        return false;
+    }
+
+    const boundingRect = canvas.getBoundingClientRect();
+
+    const scaleX = canvas.width / boundingRect.width;
+    const scaleY = canvas.height / boundingRect.height;
+
+    const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+    const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+    const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), HEIGHT - 1);
+    const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), WIDTH - 1);
+
+    board.flag(row, col);
+
+    render();
+
+    return false;
+}, false);
+
+const render = () => {
+    const ctx = canvas.getContext("2d");
+    ctx.font = "20px Courier New";
+
+    if (board.done()) {
+        done.innerText = "Done!";
+    }
+
+    drawGrid(ctx);
+    drawCells(ctx);
+};
+
+const drawGrid = (ctx) => {
+    ctx.beginPath();
+    ctx.strokeStyle = GRID_COLOR;
+
+    // Draw vertical lines
+    for (let i = 0; i <= WIDTH ; i++) {
+        ctx.moveTo(( CELL_SIZE + 1) * i  + 1, 0);
+        ctx.lineTo(( CELL_SIZE + 1) * i + 1, ( CELL_SIZE + 1 ) * HEIGHT + 1 );
+    }
+
+    // Draw horizontal lines
+    for (let i = 0; i <= HEIGHT; i++) {
+        ctx.moveTo(0, ( CELL_SIZE + 1) * i + 1);
+        ctx.lineTo(( CELL_SIZE + 1) * WIDTH + 1, ( CELL_SIZE + 1 ) * i + 1 );
+    }
+
+    ctx.stroke();
+};
+
+const drawCells = (ctx) => {
+    ctx.beginPath();
+
+    for (let row = 0; row < HEIGHT; row++) {
+        for (let col = 0; col < WIDTH; col++) {
+            const x = col * (CELL_SIZE + 1);
+            const y = row * (CELL_SIZE + 1);
+            const cell = board.get_cell(row, col);
+
+            if (cell.revealed) {
+                ctx.fillStyle = REVEALED_COLOR;
+                ctx.fillRect(
+                    x + 1,
+                    y + 1,
+                    CELL_SIZE,
+                    CELL_SIZE
+                );
+
+                ctx.fillStyle = "#000000";
+
+                if (cell.mine) {
+                    ctx.fillText("X", x+5, y+17);
+                } else {
+                    const count = cell.neighbors;
+                    ctx.fillStyle = COUNT_COLORS[count - 1];
+                    if (count > 0) {
+                        ctx.fillText("" + count, x+5, y+17);
+                    }
+                }
+            } else {
+                ctx.fillStyle = HIDDEN_COLOR;
+                ctx.fillRect(
+                    x + 1,
+                    y + 1,
+                    CELL_SIZE,
+                    CELL_SIZE
+                );
+
+                if (cell.flagged) {
+                    ctx.fillStyle = "#000000";
+                    ctx.fillText("F", x+5, y+17);
+                }
+            }
+        }
+    }
+
+    ctx.stroke();
+};
+
+render();
